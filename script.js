5fd499e57849cb5ff8a7c69973b8cf95527ac5a7
temka1218 <alcantrazalcantraz@gmail.com>
2020-08-12 11:53:09 +0800

    m

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-document.querySelector('.fdsfds').addEventListener('click')
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
+
+
+
+
