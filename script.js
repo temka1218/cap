fc7a68c1d1578a82e2c08caa7c5813caf92743eb
temka1218 <alcantrazalcantraz@gmail.com>
2020-08-12 12:05:05 +0800

    blah

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
-
-
-
-
 
