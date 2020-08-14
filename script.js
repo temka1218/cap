4f6a6475a6c5a7fd4fbda3ce0f21cced4c239430
temka1218 <alcantrazalcantraz@gmail.com>
2020-08-14 14:06:50 +0800

    shiljiltvvd + tailwind

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+var express = require("express");
+var mysql = require("mysql");
 
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "",
+});
+
+
+connection.connect(function (error) {
+  if (!!error) {
+    console.log("error");
+  } else {
+    console.log("connected");
+  }
+});
