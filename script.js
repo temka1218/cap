4ed235816d788fba862fccf6ff542a8bb96fc297
temka1218 <alcantrazalcantraz@gmail.com>
2020-08-25 11:26:19 +0800

    hmm

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var express = require("express");
 var mysql = require("mysql");
+var bodyparser = require("body-parser");
 
+var app = express();
+app.use(bodyparser.json());
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "",
+  multipleStatements: true
 });
+
 connection.connect(function (error) {
   if (!!error) {
     console.log("error");
